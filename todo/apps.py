778ee26275f7f12aa3daab6dd27cde6d7ea1f07c
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-07 04:04:29 +0000

    Add new page to add items

--- todo/apps.py
+++ todo/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class TodoConfig(AppConfig):
+    """ Config app """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo'
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    """ Create class of an Item """
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done_status = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
