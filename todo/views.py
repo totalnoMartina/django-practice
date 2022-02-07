10feb96e55489555dbd15b3ae480e68769fbbc22
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-07 13:44:11 +0000

    Remove forms until i get add item to work

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Item
 
+
 # Create your views here.
 
 
@@ -14,9 +15,4 @@ def get_todo_list(request):
 
 def add_item(request):
     """ Rendering items """
-    if request.method == 'POST':
-        name = request.POST.get('item.name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
-        return redirect('get_to_do_list')
-    return render(request, 'todo/add_items.html')
+    return render(request, 'todo/add_item.html')
