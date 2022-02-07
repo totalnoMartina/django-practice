d5329912dc394d2d730442a3542fe6339101c948
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-07 17:52:25 +0000

    Add toggle, using done_status instead item.done

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_todo_list, name='get_todo_list'),
     path('add/', views.add_item, name='add'),
-    path('edit/<item_id>', views.edit_item, name='edit')
+    path('edit/<item_id>', views.edit_item, name='edit'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle')
 ]
