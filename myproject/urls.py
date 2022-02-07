5d9591a4b0a20f9ea1b07de45823f6f92175f5c7
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-07 17:38:10 +0000

    Update items succesfully

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,6 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_todo_list, name='get_todo_list'),
-    path('add/', views.add_item, name='add')
+    path('add/', views.add_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name='edit')
 ]
