45bd255305d123acac7e2b30ca35c6b4198e8a94
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-07 21:42:56 +0000

    Add dj_database_url, migrated, coverage html

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,13 +75,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+    'default': dj_database_url.parse('postgres://lkvktwucqptvqk:fd8ab4234103be72a36e20536adcddac243c673f3c21b539604044974faee2f0@ec2-54-247-137-184.eu-west-1.compute.amazonaws.com:5432/d5eeut6n6mvsq2')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
