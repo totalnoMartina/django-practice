054339c7537a2f78ae46276d13df24215ef67af5
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-08 01:26:38 +0000

    Add os.environ.get and debug=development

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 import dj_database_url
 
+
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get( 'SECRET_KEY', 'django-insecure-5gkn+q22u=afdy0zc*yc^1gn0bco81mvaw8$gp+%91rp_*fm-%')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5gkn+q22u=afdy0zc*yc^1gn0bco81mvaw8$gp+%91rp_*fm-%')
 
 # SECURITY WARNING: don't run with debug turned on in production! // 'django-project-martina.herokuapp.com',
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+
 
 
 # Application definition
@@ -75,17 +82,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if development:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3')
+        }
+            }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
